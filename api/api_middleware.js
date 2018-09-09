45d8d3b27bb5e62bf67327bb1f792f356248ea44
Eric Jackson <ejackson@ashevillenc.gov>
2018-09-09 14:20:56 -0400

    Added superuser lookup

--- api/api_middleware.js
+++ api/api_middleware.js
@@ -1,4 +1,7 @@
 const parseurl = require('parseurl');
+const getDbConnection = require('../common/db');
+const cache = require('coa-web-cache');
+const logger = require('../common/logger');
 
 const middlewares = [
   // The following code just exercises the session and login
@@ -19,6 +22,28 @@ const middlewares = [
       console.log(`View count is ${JSON.stringify(req.session.views)} for ${sessionId}`);
       next();
     });
+  },
+  function (req, res, next) { // Get superuser status
+    if (req.session && req.session.id) {
+      cache.get(req.session.id)
+      .then(cacheData => {
+        if (cacheData && cacheData.user && cacheData.user.id) {
+          const conn = getDbConnection('aws');
+          req.session.superuser = false;
+          return conn
+          .query(`SELECT * FROM checkins.superusers WHERE employee_id = ${cacheData.user.id} limit 1`)
+          .then(result => {
+            if (result.rows.length === 1) {
+              req.session.superuser = result.rows[0].is_superuser !== 0;
+              if (req.session.superuser) logger.warn(`Superuser login by ${cacheData.user.email}'`);
+            }
+            next();
+          });
+        } else {
+          next();
+        }
+      });
+    }
   },
 ];
 
--- api/db_configs.js
+++ api/db_configs.js
@@ -1,1 +1,20 @@
-module.exports = {};
+module.exports = {
+  aws: {
+    db_type: 'pg',
+    host: process.env.aws_host,
+    user: process.env.aws_user,
+    password: process.env.aws_password,
+    database: process.env.aws_database,
+    port: 5432,
+    ssl: false,
+  },
+  mds: {
+    db_type: 'pg',
+    host: process.env.mds_host,
+    user: process.env.mds_user,
+    password: process.env.mds_password,
+    database: process.env.mds_database,
+    port: 5432,
+    ssl: false,    
+  }
+};
